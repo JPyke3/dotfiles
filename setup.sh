13b1fa8c323f4d47403134f04d95f2dda8bf0d1f
JPyke3 <pyke.jacob1@gmail.com>
2020-09-02 13:57:24 +1000

    Updated install script

--- setup.sh
+++ setup.sh
@@ -1,20 +1,106 @@
 #!/bin/bash
+# Vars
+RED='\033[0;31m'
+NC='\033[0m'
 
-# Install Oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Defaults
+export RUNZSH=no
 
+# Install some software
+if [ -f /bin/pacman ]; then
+    echo -e "${RED}Arch Linux Detected${NC}"
+    sudo pacman -S neofetch zsh emacs vim
+elif [ -f /bin/apt ]; then
+    echo -e "${RED}Debian Variant Detected${NC}"
+    sudo apt install neofetch zsh emacs vim
+elif [ -f /usr/local/bin/brew ]; then
+    echo -e "${RED}MacOS Detected${NC}"
+    brew install neofetch zsh emacs vim
+else
+    echo -e "${RED}This OS Isn't Supported, aborting!${NC}"
+fi
+
+# Move Existing Configs
+echo -e "${RED}Backing up existing config files${NC}"
+mv .spacemacs .spacemacs.old
+mv .zshrc .zshrc.old
+mv .vimrc .vimrc.old
+
+# Install Oh my zsh - in case it isn't
+if [ -d ~/.oh-my-zsh ]; then
+    echo -e "${RED}Oh My Zsh is installed!${NC}"
+else
+    echo -e "${RED}Installing Oh My Zsh${NC}"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Zsh addons
+echo -e "${RED}Installing some Zsh plugins${NC}"
 # Install zsh autocomplete
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 # Install powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
+# Install Spacemacs
+if [ -d ~/.emacs.d ]; then
+    if [ -f ~/.emacs.d/spacemacs.mk ]; then
+        echo -e "${RED}Spacemacs already installed!${NC}"
+    else
+        echo -e "${RED}Emacs Installed but no spacemacs, backing up emacs"
+        mv ~/.emacs.d ~/.emacs.d.old
+        git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+        cd ~/.emacs.d
+        git checkout develop
+        cd ~/.
+    fi
+else
+    echo -e "${RED}Installing Spacemacs${NC}"
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+    cd ~/.emacs.d
+    git checkout develop
+    cd ~/.
+fi
+
+# Obtain all dotfiles
+echo -e "${RED}Retreiving Dots!${NC}"
+rm -rf ~/dotfiles
+cd ~/.
+git clone https://github.com/JPyke3/dotfiles
+
+# Move all dotfiles
+echo -e "${RED}Moving Dots!${NC}"
+for file in * ~/dotfiles/.*
+do
+    test -f "$file" && mv "$file" "$HOME"
+done
+
+# Make i3 dir
+echo -e "${RED}Configuring i3${NC}"
+mkdir ~/.config/i3 -p
+
+# Move and backup i3 Config
+if [ -f ~/.config/i3/config ]; then
+    mv ~/.config/i3/config ~/.config/i3/config.old
+fi
+mv ~/dotfiles/.config/i3/config ~/.config/i3/config
+
 # Make vim dir
+echo -e "${RED}Configuring Vim${NC}"
 mkdir ~/.vim/undodir -p
 
 # Vim Plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +'PlugInstall --sync' +qa
+cd ~/.vim/plugged/YouCompleteMe
+python3 install.py --all
+cd ~/. 
 
-# Obtain all dotfiles
-git clone https://github.com/JPyke3/dotfiles
-mv ./dotfiles/.* ~/.
+# Clean Up
+echo -e "${RED}Cleaning Up${NC}"
+rm -rf ~/dotfiles
+
+# New shell to get new configs
+clear
+/bin/neofetch
+echo -e "${RED}Installation Successful${NC}"
+exec /bin/zsh
